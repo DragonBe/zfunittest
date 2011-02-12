f4811ec3a7d861be31204ae8112bc7ec34ccac9e
Michelangelo <dragonbe+github@gmail.com>
2011-02-12 13:55:08 +0100

    Setting up a testsuite for connecting with the world

---
+++ library/My/Exception.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * My library
+ *
+ * My custom library to override or add new functionality to this
+ * demo application
+ *
+ * @package		My
+ * @license		CreativeCommons-Attribution-ShareAlike
+ * @link		http://creativecommons.org/licenses/by-sa/3.0/
+ */
+/**
+ * @see			Zend_Exception
+ */
+/**
+ * My_Exception
+ *
+ * All exceptions raised by this framework should extend from this
+ * base exception class.
+ */
+class My_Exception extends Zend_Exception
+{
+}
---
+++ tests/application/controllers/ConnectControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+class ConnectControllerTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        /* Setup Routine */
+    }
+
+    public function tearDown()
+    {
+        /* Tear Down Routine */
+    }
+
+    public function testSiteCanConnectWithTwitter()
+    {
+        $this->markTestIncomplete('Not yet implemented');
+    }
+    
+    public function testSiteCanConnectWithFacebook()
+    {
+        $this->markTestIncomplete('Not yet implemented');
+    }
+    
+    public function testSiteCanConnectWithGoogle()
+    {
+        $this->markTestIncomplete('Not yet implemented');
+    }
+}
+
